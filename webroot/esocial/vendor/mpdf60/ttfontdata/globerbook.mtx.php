83efd3b54c35e3c2aa87f513a30120f6b6755345
SeconciDF <ti.seconcidf@seconci-df.org.br>
2024-10-17 16:20:28 -0300

    Add files via upload

---
+++ webroot/esocial/vendor/mpdf60/ttfontdata/globerbook.mtx.php
@@ -0,0 +1,77 @@
+<?php
+$name='Glober-Book';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 720,
+  'XHeight' => 526,
+  'FontBBox' => '[-57 -252 915 930]',
+  'Flags' => 4,
+  'Ascent' => 930,
+  'Descent' => -252,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 365,
+);
+$unitsPerEm=2048;
+$up=-174;
+$ut=50;
+$strp=315;
+$strs=50;
+$ttffile='/var/www/html/sipro/vendor/mpdf60/ttfonts/Glober-Book.ttf';
+$TTCfontID='0';
+$originalsize=48800;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='globerbook';
+$panose=' 0 0 0 0 0 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=true;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 750, -250, 0
+// usWinAscent/usWinDescent = 1052, -270
+// hhea Ascent/Descent/LineGap = 750, -250, 0
+$useOTL=255;
+$rtlPUAstr='';
+$GPOSScriptLang=array (
+  'DFLT' => 'DFLT ',
+  'latn' => 'DFLT ',
+);
+$GPOSFeatures=array (
+  'DFLT' => 
+  array (
+    'DFLT' => 
+    array (
+      'kern' => 
+      array (
+        0 => 0,
+      ),
+    ),
+  ),
+  'latn' => 
+  array (
+    'DFLT' => 
+    array (
+      'kern' => 
+      array (
+        0 => 0,
+      ),
+    ),
+  ),
+);
+$GPOSLookups=array (
+  0 => 
+  array (
+    'Type' => 2,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 31066,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+);
+?>
